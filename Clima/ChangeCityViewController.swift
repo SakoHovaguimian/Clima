41726523a224f285a95ad4972a415f8b4ab8800c
Sako Hovaguimian <sakohovaguimian@yahoo.com>
2018-07-09 22:14:58 -0700

    Weather App Final Version

---
+++ Clima/ChangeCityViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ChangeCityViewController.swift
+//  WeatherApp
+//
+//  Created by Sako Hovaguimian on 23/08/2018.
+//  Copyright (c) 2018 Sako Hovaguimian. All rights reserved.
+//
+
+import UIKit
+
+
+
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName(city: String)
+}
+
+
+class ChangeCityViewController: UIViewController {
+    
+    
+    var delegate : ChangeCityDelegate?
+    
+    
+    @IBOutlet weak var changeCityTextField: UITextField!
+
+    
+    
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+    
+        
+        
+       
+        let cityName = changeCityTextField.text!
+        
+ 
+        delegate?.userEnteredANewCityName(city: cityName)
+        
+       
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+
+   
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
